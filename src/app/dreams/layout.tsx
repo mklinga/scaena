5e4bbfb264fa9a07eefa6e785a95e4e3a7c27123
mklinga <mail@mklinga.com>
2024-04-24 21:31:18 +0300

    Add breadcrumbs to dream page

---
+++ src/app/dreams/layout.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function DreamsLayout({ children }: { children: ReactNode }) {
+    return (
+        <div>
+            <nav className="p-3">
+                <Link href="/">root</Link>
+                <span> - Dreams</span>
+            </nav>
+            {children}
+        </div>
+    )
+}
